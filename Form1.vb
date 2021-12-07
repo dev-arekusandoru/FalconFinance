22cf5691204f1a71b6fe794d101f14d3adbb1bab
kfirestone25 <kf1322@messiah.edu>
2021-12-07 17:10:57 -0500

    changes

--- Form1.vb
+++ Form1.vb
@@ -11,16 +11,19 @@
     ' Show the Currency Converter form when button is clicked
     Private Sub btnCurrencyConverter_Click(sender As Object, e As EventArgs) Handles btnCurrencyConverter.Click
         CurrencyConverter.Show()
+        Me.Hide()
     End Sub
 
     ' Show the Loan Calculator form when button is clicked
     Private Sub btnLoanCalculator_Click(sender As Object, e As EventArgs) Handles btnLoanCalculator.Click
         LoanCalculator.Show()
+        Me.Hide()
     End Sub
 
     ' Show the Interest Calculator form when button is clicked
     Private Sub btnInterestCalculator_Click(sender As Object, e As EventArgs) Handles btnInterestCalculator.Click
         InterestCalculator.Show()
+        Me.Hide()
     End Sub
 
     ' Close the form when Exit button is clicked
@@ -29,6 +32,20 @@
     End Sub
 
     Private Sub btnDeposit_Click(sender As Object, e As EventArgs) Handles btnDeposit.Click
+        Dim strAccount As String = InputBox("What account?")
+        Dim strMoney As String = InputBox("Amount?")
+        Dim dblCheckingBalance As Integer
+        Dim dblSavingsBalance As Integer
+        Dim dblEmergencyBalance As Integer
 
+        Integer.TryParse()
+
+        If strAccount = "Checking" Or "checking" Then
+            dblCheckingBalance = dblCheckingBalance + strMoney
+        ElseIf strAccount = "Savings" Or "savings" Then
+            dblSavingsBalance = dblSavingsBalance + strMoney
+        ElseIf strAccount = "Emergency Fund" Or "emergency fund" Or "Emergency fund" Then
+            dblEmergencyBalance = dblEmergencyBalance + strMoney
+        End If
     End Sub
 End Class
